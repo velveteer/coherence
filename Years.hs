0419848476e2f795fc3613a92b94fb62d287d4aa
Josh Miller <notjoshmiller@gmail.com>
2021-06-08 18:53:18 -0500

    Rename module

--- Years.hs
+++ Years.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# OPTIONS_GHC -Wno-missing-methods #-}
 
-module Coherence where
+module Years where
 
 newtype Months = Months Integer
   deriving (Eq, Show, Num, Integral, Enum, Ord, Real)
